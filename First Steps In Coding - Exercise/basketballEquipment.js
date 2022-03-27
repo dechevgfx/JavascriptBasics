ad5bc7db9376b0251777f51d54e8d52667b485be
dechevgfx <101424579+dechevgfx@users.noreply.github.com>
2022-03-27 20:37:19 +0300

    Add files via upload

---
+++ First Steps In Coding - Exercise/basketballEquipment.js
@@ -0,0 +1,17 @@
+function basketballEq(input){
+    
+    let priceOfTraining = Number(input[0]);
+    
+    let shoes = (priceOfTraining * 0.60);
+    let dress = (shoes * 0.80);
+    let ball = (dress * (1 / 4));
+    let accesories = (ball * (1 / 5));
+
+    let totalCost = (priceOfTraining + shoes + dress + ball + accesories);
+    
+    console.log(totalCost);
+
+
+}
+
+basketballEq([550]);
---
+++ First Steps In Coding - Exercise/usdBgn.js
@@ -0,0 +1,7 @@
+function usdBgn(input){
+    let usd = 1.79549;
+    let bgn = (input[0]);
+    let usdbgn = usd * bgn;
+    console.log(usdbgn);
+}
+usdBgn(["22"])
